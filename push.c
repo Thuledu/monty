a6d11623efbdcd900c7e4aab62d828917c431f36
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-08-27 12:30:24 -0700

    Stacks, Queues - LIFO, FIFO Tasks

--- push.c
+++ push.c
@@ -1,43 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "monty.h"
 
-/**
- * push_opcode - adds node to the stack
- * @head: stack head
- * @counter: line_number
- * Return: void
- */
-void push_opcode(stack_t **head, unsigned int counter)
-{
-	int n;
-	int k = 0, flag = 0;
-
-
-	if (!bus.arg)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", counter);
-		exit(EXIT_FAILURE);
-	}
-	if (bus.arg[0] == '-')
-		k++;
-	for (; bus.arg[k] != '\0'; k++)
-	{
-		if (bus.arg[k] > '9' || bus.arg[k] < '0')
-		{
-			flag = 1;
-			break;
-		}
-	}
-	if (flag == 1)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", counter);
-		exit(EXIT_FAILURE);
-	}
-
-	n = atoi(bus.arg);
-	if (bus.lifi == 0)
-		addnode(head, n);
-	else
-		addqueue(head, n);
+void push(int value) {
+    printf("Pushed value: %d\n", value);
 }
+
+int main(int argc, char *argv[]) {
+
+	char *endptr = NULL;
+	int value;
+
+	if (argc != 2) {
+        fprintf(stderr, "Error: L%d: Usage: %s <int>\n", __LINE__, argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    
+
+    if (!isdigit(argv[1][0])) {
+        fprintf(stderr, "Error: L%d: Usage: %s integer\n", __LINE__, argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    value = strtol(argv[1], &endptr, 10);
+
+    if (*endptr != '\0') {
+        fprintf(stderr, "Error: L%d: Usage: %s integer\n", __LINE__, argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    push(value);
+
+    return 0;
+}
+
